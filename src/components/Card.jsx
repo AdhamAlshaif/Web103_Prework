c0a1e3b88f9eb599c0cc6e422e27187e72897492
Adham <adhamalshaif@gmail.com>
2025-08-24 01:24:28 -0400

    form+card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./card.css";
+import { FaInstagram } from "react-icons/fa";
 
+/*
+what i have to do here:
+1. create a card that will show creator info
+2. add the css.
 
+*/ 
 const Card = () => {
   return (
-   <div className='container'>
-    <div className='card'>
+    <>
+      <div className="card">
+        <h1>Adham</h1>
+        <a
+          href="https://instagram.com/yourhandle"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://instagram.com/yourhandle"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://instagram.com/yourhandle"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon instagram"
+        >
+          <FaInstagram />
+        </a>
+      </div>
+    </>
+  );
+};
 
-    </div>
-
-   </div>
-  )
-}
-
-export default Card
+export default Card;
--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const AddCreator = () => {
   return (
-    <div></div>
+    <>
+      <div className="creator-form">
+        <form>
+          <div className="form-group1">
+            <label htmlFor="creatorName">Creator Name:</label>
+            <input type="text" id="creatorName" />
+            <label htmlFor="image">Image URL:</label>
+            <input type="text" id="image" />
+            <label htmlFor="description">Description:</label>
+            <textarea id="description"></textarea>
+          </div>
+          <div className="form-group2">
+            <h3>Social Media Links:</h3>
+            <label htmlFor="Twitch">Twitch:</label>
+            <input type="text" id="Twitch" />
+            <label htmlFor="YouTube">YouTube:</label>
+            <input type="text" id="YouTube" />
+            <label htmlFor="Instagram">Instagram:</label>
+            <input type="text" id="Instagram" />
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
